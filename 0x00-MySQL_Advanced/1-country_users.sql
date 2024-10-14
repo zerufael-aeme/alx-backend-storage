943955987cbd1a3dca11d768da2b28e3c253e814
zerufael <zerufaelaemero@gmail.com>
2024-10-14 12:38:20 -0700

    ENUM

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Creates a table with unique users.
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255)
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
