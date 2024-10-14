cd641ae8bd8d70adfbaed7fc306ff619fd2afa59
zerufael <zerufaelaemero@gmail.com>
2024-10-14 12:32:58 -0700

    create_table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Creates a table with unique users.
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255)
+);
