fe4045b2099b2fbe3657c285b980183645e65816
zerufael <zerufaelaemero@gmail.com>
2024-10-15 13:52:56 -0700

    Triggers

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,11 +4,13 @@ DROP TRIGGER IF EXISTS validate_email;
 DELIMITER $$
 
 CREATE TRIGGER validate_email
-AFTER UPDATE ON users
+BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0;
+    ELSE
+	SET NEW.valid_email = NEW.valid_email;
     END IF;
 END $$
 DELIMITER ;
