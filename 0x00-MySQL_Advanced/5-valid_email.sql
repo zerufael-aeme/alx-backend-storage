cc69574cfbf6646018d88f7d68c98e1cf17d404a
zerufael <zerufaelaemero@gmail.com>
2024-10-15 13:44:18 -0700

    Trigger

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -7,7 +7,7 @@ CREATE TRIGGER validate_email
 AFTER UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
+    IF NEW.email != OLD.email THEN
         UPDATE users
         SET valid_email = 0
         WHERE id = NEW.id;
