f65b788ceffcafa152de0121a750e63b9a3fd18a
zerufael <zerufaelaemero@gmail.com>
2024-10-15 12:01:33 -0700

    IFNULL

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam Rock'
+ORDER BY lifespan DESC;
