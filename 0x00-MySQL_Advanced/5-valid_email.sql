98212dc9b69111cecc0315f511a48882e59ba3c4
zerufael <zerufaelaemero@gmail.com>
2024-10-15 13:41:08 -0700

    Triggers

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,17 @@
+-- Creates a trigger that resets the attribute valid_email
+-- only when the email has been changed.
+DROP TRIGGER IF EXISTS validate_email;
+DELIMITER $$
+
+CREATE TRIGGER validate_email
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        UPDATE users
+        SET valid_email = 0
+        WHERE id = NEW.id;
+    END IF;
+END $$
+
+DELIMITER ;
