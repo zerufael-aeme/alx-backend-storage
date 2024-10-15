d861c8ba8be179095560e7546b41240da4338e28
zerufael <zerufaelaemero@gmail.com>
2024-10-15 13:01:03 -0700

    Triggers

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,7 @@
 -- Creates a trigger that decreases the quantity
 -- of an item after adding a new order.
+DROP TRIGGER IF EXISTS Dec_quantity;
+DELIMITER $$
 CREATE TRIGGER Dec_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
@@ -7,4 +9,5 @@ BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
-END;
+END $$
+DELIMITER ;
