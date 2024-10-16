8128d21cdc728bf278feda1c4dad4acac110924d
zerufael <zerufaelaemero@gmail.com>
2024-10-16 10:10:08 -0700

    AVG

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,16 +3,18 @@
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
     DECLARE average FLOAT;
-    SELECT AVG(score) INTO average
-    FROM corrections
-    WHERE user_id = user_id;
+
+    SELECT AVG(score)
+       INTO average
+       FROM corrections
+       WHERE corrections.user_id = user_id;
 
     UPDATE users
-    SET average_score = average
-    WHERE id = user_id;
+       SET users.average_score = average
+       WHERE id = user_id;
 END $$
 
 DELIMITER ;
