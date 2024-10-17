73d5decbd46984a6bd917864820232f6a56bc595
zerufael <zerufaelaemero@gmail.com>
2024-10-17 09:22:36 -0700

    VIEWS

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,7 @@
 -- creates a function SafeDiv that divides (and returns) the first
 -- by the second number or returns 0 if the second number is equal to 0.
-CREATE VIEW  need_meeting
-AS SELECT * FROM students
-WHERE score < 80 AND (last_meeting IS NULL OR DATEDIFF(CURDATE(), last_meeting) > 30);
+CREATE VIEW  need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+AND (last_meeting IS NULL OR DATEDIFF(CURDATE(), last_meeting) > 30);
